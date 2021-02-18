acdf1fc3292740c0a34b55ae6bc558cf6cb315b7
Vladyslav-Vakalyuk <vlad.vakalyk@gmail.com>
2021-02-18 23:08:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,1 @@
-<?php
-function reverse($head) {
-$newDLL = new DoublyLinkedList();
-        while ($head){
-    $newDLL->insertNode($head->data);
-    $head = $head->next;
-     
-    }
-    $newDLLDone = new DoublyLinkedList();
-    $data = $newDLL->tail;
-        while ($data){
-    $newDLLDone->insertNode($data->data);
-    $data = $data->prev;
-    
-    }
-   
-    return $newDLLDone->head;
 
-}   
