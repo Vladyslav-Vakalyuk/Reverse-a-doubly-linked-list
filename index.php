71cc96d6d9ec1b8c91eea810bddd1d543e86481c
Vladyslav-Vakalyuk <vlad.vakalyk@gmail.com>
2021-02-18 23:08:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,87 @@
 
+<?php
+
+class DoublyLinkedListNode {
+    public $data;
+    public $next;
+    public $prev;
+
+    public function __construct($node_data)
+    {
+        $this->data = $node_data;
+        $this->next = null;
+        $this->prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    public $head;
+    public $tail;
+
+    public function __construct()
+    {
+        $this->head = null;
+        $this->tail = null;
+    }
+
+    public function insertNode($node_data)
+    {
+        $node = new DoublyLinkedListNode($node_data);
+
+        if (is_null($this->head)) {
+            $this->head = $node;
+        } else {
+            $this->tail->next = $node;
+            $node->prev = $this->tail;
+        }
+
+        $this->tail = $node;
+    }
+}
+
+function printDoublyLinkedList($node, $sep, $fptr)
+{
+    while (!is_null($node)) {
+        fwrite($fptr, $node->data);
+
+
+        $node = $node->next;
+
+        if (!is_null($node)) {
+            fwrite($fptr, $sep);
+        }
+    }
+}
+
+// Complete the reverse function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode next;
+ *     DoublyLinkedListNode prev;
+ * }
+ *
+ */
+function reverse($head) {
+$newDLL = new DoublyLinkedList();
+        while ($head){
+    $newDLL->insertNode($head->data);
+    $head = $head->next;
+     
+    }
+    $newDLLDone = new DoublyLinkedList();
+    $data = $newDLL->tail;
+        while ($data){
+    $newDLLDone->insertNode($data->data);
+    $data = $data->prev;
+    
+    }
+   
+    return $newDLLDone->head;
+
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
